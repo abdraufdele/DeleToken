32fdf39356f129d611cb0a7914f37c1916137026
abdrauf <deleabdulrauf@gmail.com>
2024-10-21 00:45:16 +0100

    first commit

--- test/DeleTokenTest..t.sol
+++ test/DeleTokenTest..t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.26;
 
-import{Test} from "forge-std/Test.sol";
-import{DeleToken} from "../src/DeleToken.sol";
-import{DeployDeleToken} from "../script/DeployDeleToken.s.sol";
+import {Test} from "forge-std/Test.sol";
+import {DeleToken} from "../src/DeleToken.sol";
+import {DeployDeleToken} from "../script/DeployDeleToken.s.sol";
 
 contract DeleTokenTest is Test {
     DeleToken public deletoken;
@@ -20,11 +20,12 @@ contract DeleTokenTest is Test {
 
         vm.prank(msg.sender);
         deletoken.transfer(bob, STARTING_BALANCE);
-
     }
+
     function testBobBalance() public view {
         assertEq(STARTING_BALANCE, deletoken.balanceOf(bob));
     }
+
     function testAllowances() public {
         uint256 initialAllowance = 1000;
         //Bob approves alice to spend tokens
@@ -38,6 +39,7 @@ contract DeleTokenTest is Test {
         assertEq(deletoken.balanceOf(alice), transferAmount);
         assertEq(deletoken.balanceOf(bob), STARTING_BALANCE - transferAmount);
     }
+
     function testTransfer() public {
         uint256 amount = 1000;
         address receiver = address(0x1);
@@ -45,14 +47,16 @@ contract DeleTokenTest is Test {
         deletoken.transfer(receiver, amount);
         assertEq(deletoken.balanceOf(receiver), amount);
     }
+
     function testBalanceAfterTransfer() public {
-        uint256 amount= 1000;
+        uint256 amount = 1000;
         address receiver = address(0x1);
         uint256 initialBalance = deletoken.balanceOf(msg.sender);
         vm.prank(msg.sender);
         deletoken.transfer(receiver, amount);
         assertEq(deletoken.balanceOf(msg.sender), initialBalance - amount);
     }
+
     function testTransferFrom() public {
         uint256 amount = 1000;
         address receiver = address(0x1);
@@ -62,3 +66,4 @@ contract DeleTokenTest is Test {
         assertEq(deletoken.balanceOf(receiver), amount);
     }
 }
+////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
